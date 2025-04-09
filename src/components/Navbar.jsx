7e48b65f47648adf9d3994868672de1c6f118f51
sabnam59 <sabukarke1@gmail.com>
2025-04-09 17:27:41 +0545

    responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,46 +5,72 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   const [hamburgerMenu, sethamburgerMenu] = useState(false);
 
+  console.log(hamburgerMenu);
+
   const handleClick = () => {
     sethamburgerMenu(!hamburgerMenu);
   };
+  
 
   return (
     <>
-      <nav className="sticky top-0 z-50 bg-black text-white">
-        {/* Desktop */}
-        <div className="bg-black text-white lg:justify-between xl:pl-40 pl-20 pr-10 xl:pr-16 md:justify-evenly shadow py-10 hidden md:flex">
-          <div className="bold text-5xl lg:text-4xl flex items-center md:hidden lg:flex">
+      <nav className="sticky top-0 z-50 bg-black text-white ">
+        <div
+          className="bg-black text-white shadow py-10 px-5 md:pl-20 md:pr-10 
+          flex justify-between md:justify-evenly md:flex lg:justify-between xl:pl-40 xl:pr-16 "
+        >
+          <div className="bold items-center flex text-xl lg:text-4xl xl:text-5xl  md:hidden lg:flex">
             Sabnam
           </div>
 
-          <div className="md:flex gap-10 uppercase sm:hidden  text-white xl:text-xl">
-            <Link to="/" className="hover:text-amber-400 self-center">
+          <div className="sm:hidden" onClick={handleClick}>
+            <Menu />
+          </div>  
+
+          <div
+            className={`absolute top-20 right-0 flex flex-col p-5 md:w-full font-semibold gap-4 transition-all md:flex-row
+              md:items-center  
+              ${
+                hamburgerMenu ? "block bg-white text-black" : "hidden md:flex"
+              }`
+            }
+          >
+            <Link
+              to="/"
+              className="hover:text-amber-400 self-center   "
+              onClick={handleClick}
+            >
               Home
             </Link>
             <Link
               to="/about"
               className="hover:text-amber-400 self-center"
+              onClick={handleClick}
             >
               About
             </Link>
-            <Link to="/gallery" className="hover:text-amber-400 self-center">
+            <Link
+              to="/gallery"
+              onClick={handleClick}
+              className="hover:text-amber-400 self-center "
+            >
               Gallery
             </Link>
 
             <div className="relative group self-center flex">
-              <Link className="hover:text-amber-400 flex self-center" to="#">
+              <Link to="#" className="hover:text-amber-400 self-center ">
                 Pages
                 <i className="fa-solid fa-angle-down ml-1 self-center"></i>
               </Link>
 
               {/* <!-- PAGES DROPDOWN ON HOVER --> */}
               <div className="absolute border rounded-lg mt-o.5 z-10 left-0 top-full hidden w-25 bg-white text-black  group-hover:block">
-                <ul className="py-2 text-sm lg:text-xl">
+                <ul className="py-3 text-sm lg:text-xl">
                   <li>
                     <Link
+                      onClick={handleClick}
                       to="/pages/blog"
-                      className="block hover:bg-gray-300 hover:font-semibold px-4 py-2 border-b-dashed border-black"
+                      className="block  hover:bg-gray-300 hover:font-semibold px-4 py-3 border-b-dashed border-black"
                     >
                       Blog
                     </Link>
@@ -52,6 +78,7 @@ const Navbar = () => {
                   <li>
                     <Link
                       to="/pages/project"
+                      onClick={handleClick}
                       className="block hover:font-semibold hover:bg-gray-300 px-5 py-3 "
                     >
                       Project
@@ -63,40 +90,19 @@ const Navbar = () => {
 
             <Link
               to="/contact"
+              onClick={handleClick}
               className="py-2 hover:text-amber-500 self-center"
+            ></Link>
+
+            <button
+              onClick={handleClick}
+              className="items-center cursor-pointer transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110"
             >
-              Contact
-            </Link>
-            <button className="flex items-center cursor-pointer transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110">
-              <span className=" border-2 border-yellow-600 px-5 py-2 rounded-sm hover:text-amber-400">
+              <span className=" border-2 border-yellow-600 px-5 py-2 rounded-md hover:text-amber-400">
                 Hire Me
                 <i className="fa-solid fa-angles-right"></i>
               </span>
             </button>
-          </div>
-        </div>
-
-        {/* mobile */}
-        <div className="flex justify-between sm:hidden p-5 list-none sticky top-0 z-100">
-          <div className="text-xl">Sabnam</div>
-
-          <ul className="hidden md:flex space-x-5">
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-          </ul>
-
-          <div className="sm:hidden" onClick={handleClick}>
-            <Menu />
-            {hamburgerMenu ? (
-              <ul className="flex flex-col">
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
-              </ul>
-            ) : (
-              ""
-            )}
           </div>
         </div>
       </nav>
